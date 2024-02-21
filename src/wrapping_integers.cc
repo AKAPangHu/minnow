0de0a1eed45de3c70c00333f6d51558a9b688fbb
wumingyang <wumingyang@didiglobal.com>
2024-02-22 00:59:13 +0800

    fix-bug：implement wrap and unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -12,23 +12,25 @@ uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
 
   uint64_t r;
+  uint64_t offset;
+
 
   //计算出offset。因为绝对值必然是新值在后，zero值在前。所以差值固定。
   if ( raw_value_ >= zero_point.raw_value_ ) {
-    r = checkpoint + raw_value_ - zero_point.raw_value_;
+    offset = checkpoint + raw_value_ - zero_point.raw_value_;
   } else {
-    r = checkpoint + ( 1ULL << 32 ) + raw_value_ - zero_point.raw_value_;
+    offset = checkpoint + ( 1ULL << 32 ) + raw_value_ - zero_point.raw_value_;
   }
 
   //增加checkpoint的值
-  r += checkpoint;
+  r = offset + checkpoint;
 
-  // 如果数据是在checkpoint之后，那么到这步就已经结束了，因为只需要加上checkpoint的值就可以了。
-  // 但是由于数据有可能在checkpoint之前，所以需要额外校验。
+  // 如果数据规定是在checkpoint之后，那么到这步就已经结束了，因为只需要加上checkpoint的值就可以了。
+  // 但是由于数据有可能在checkpoint之前，所以需要额外校验。又因为绝对值不是在上一循环，就是在下一循环，所以直接减去一个循环的值的就可以了。
   //
-  // 1.有可能在checkpoint之前，所以要检验一下在checkpoint左边还是在右边，选择差值比较小的。
-  // 2.在第一个checkpoint边界到达之前，没有左边
-  if ( raw_value_ >= ( 1ULL << 31 ) && r >= ( 1ULL << 32 ) ) {
+  // 1.如果offset + 0 > 二分之一循环最大值，这样的值不可能在A+1循环中离上一个checkpoint点是最近的。所以可以反向证明出来不符合条件的值属于上一循环。
+  // 2.第一个循环不适用上一规则
+  if ( offset >= ( 1ULL << 31 ) && checkpoint >= ( 1ULL << 32 ) ) {
     r -= ( 1ULL << 32 );
   }
 
