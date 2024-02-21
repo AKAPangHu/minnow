62ca605c9ebcb3f5657c14af3f91db4b54d73a4e
wumingyang <wumingyang@didiglobal.com>
2024-02-21 20:32:52 +0800

    implement wrap and unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -13,7 +13,7 @@ uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
   uint64_t result;
 
   if (raw_value_ >= zero_point.raw_value_) {
-    result = checkpoint + raw_value_ - zero_point.raw_value_ + 1;
+    result = checkpoint + raw_value_ - zero_point.raw_value_;
   } else {
     result = checkpoint + (1ULL << 32) + raw_value_ - zero_point.raw_value_;
   }
