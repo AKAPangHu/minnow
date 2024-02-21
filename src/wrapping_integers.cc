a39cd8a8aef608c7023bb6260a4bd341150b97e5
wumingyang <wumingyang@didiglobal.com>
2024-02-21 21:50:51 +0800

    implement wrap and unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -2,17 +2,29 @@
 
 using namespace std;
 
-
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  return Wrap32 { (uint32_t) (n + zero_point.raw_value_ % (1ULL << 32))};
+  return Wrap32 { (uint32_t)( n + zero_point.raw_value_ % ( 1ULL << 32 ) ) };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  if (raw_value_ >= zero_point.raw_value_) {
-    return checkpoint + raw_value_ - zero_point.raw_value_;
+  uint64_t r;
+  if ( raw_value_ >= zero_point.raw_value_ ) {
+    r = checkpoint + raw_value_ - zero_point.raw_value_;
+  } else {
+    r = checkpoint + ( 1ULL << 32 ) + raw_value_ - zero_point.raw_value_;
   }
 
-  return checkpoint + (1ULL << 32) + raw_value_ - zero_point.raw_value_;
+  if ( checkpoint == 0 ) {
+    return r;
+  }
+
+  if ( r == checkpoint ) {
+    return r;
+  } else if ( r > checkpoint ) {
+    return r + 1;
+  } else {
+    return r - 1;
+  }
 }
