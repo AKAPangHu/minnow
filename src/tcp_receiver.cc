5e5a9a0419c03a78962a0377a73e93728f5d4579
wumingyang <wumingyang@didiglobal.com>
2024-02-24 22:44:46 +0800

    add rst flag and discard confuse segment

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -6,22 +6,21 @@ using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
+
   if ( message.SYN ) {
     syn_seqno_ = message.seqno;
-  } else {
-
-    if ( !syn_seqno_.has_value() ) {
-      rst_flag_ = true;
-      return;
-    };
-
-    reassembler_.insert( message.seqno.unwrap( syn_seqno_.value(), received_byte_ ), message.payload, message.FIN );
   }
 
   if ( message.FIN ) {
     fin_flag_ = true;
   }
 
+  if ( !syn_seqno_.has_value() ) {
+    rst_flag_ = true;
+    return;
+  };
+
+  reassembler_.insert( message.seqno.unwrap( syn_seqno_.value(), received_byte_ ), message.payload, message.FIN );
   received_byte_ += message.sequence_length();
 }
 
@@ -30,8 +29,9 @@ TCPReceiverMessage TCPReceiver::send() const
 
   uint64_t ack_no = syn_seqno_.has_value() + reader().bytes_popped() + reader().bytes_buffered() + fin_flag_;
 
-  return {
-    TCPReceiverMessage { syn_seqno_.has_value() ? make_optional<>(Wrap32::wrap( ack_no, syn_seqno_.value())) : nullopt,
-                         static_cast<uint16_t>(writer().available_capacity() > UINT16_MAX ? UINT16_MAX : writer().available_capacity()),
-                         rst_flag_ } };
+  return { TCPReceiverMessage {
+    syn_seqno_.has_value() ? make_optional<>( Wrap32::wrap( ack_no, syn_seqno_.value() ) ) : nullopt,
+    static_cast<uint16_t>( writer().available_capacity() > UINT16_MAX ? UINT16_MAX
+                                                                      : writer().available_capacity() ),
+    rst_flag_ } };
 }
