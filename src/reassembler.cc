36b3b33409118f09eeeed38c8e1bb03d0c067cb8
wumingyang <wumingyang@didiglobal.com>
2024-02-29 14:18:59 +0800

    fix iterator

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -87,7 +87,7 @@ void Reassembler::insert_into_internal( uint64_t first_index, string& data )
     }
 
     // 新字符串包裹住旧字符串，删除旧字符串
-    else if ( first_index <= it->first && end_index > item_end_index ) {
+    else if ( first_index <= it->first && end_index >= item_end_index ) {
       it = internal_storage_new.erase( it );
     }
 
@@ -97,7 +97,7 @@ void Reassembler::insert_into_internal( uint64_t first_index, string& data )
     }
 
     // 新字符串右侧重叠
-    else if ( first_index <= item_end_index && end_index > item_end_index) {
+    else if ( first_index <= item_end_index && end_index >= item_end_index) {
       auto p = merge( first_index, data, it->first, it->second );
       first_index = p.first;
       assert(p.first == it->first);
